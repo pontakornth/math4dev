da6f26e22a9e1f62ff5484be9497f1e05a15968f
pontakornth <pontakorn61@gmail.com>
2021-01-03 16:30:03 +0700

    chore: Upgrade to Tailwind 2 and PostCSS 8

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 module.exports = {
   purge: ['./src/**/*.ejs', './src/**/*.md'],
+  darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
         display: ['"Kanit"', 'sans-serif'],
         body: ['"Trirong"', 'sans-serif'],
+      },
+      colors: {
+        dark: '#424242',
+        light: '#E6FFFE',
       },
     },
   },
